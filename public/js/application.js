b87873312d157acd3725665dd4253d57a60e943b
codeamac02 <codeamac02@gmail.com>
2015-07-07 11:56:23 -0500

    Add functions addForm and addComment.

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,34 @@
-// $(document).ready(function() {
-	// Este código corre después de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
-
-// });
-
-// Una manera corta de escribir document.ready
 $(function(){
+	
+	$('#new_comment_button').on("click", function(){
+		$('#new_comment_button').hide();
+		addForm();
+
+	});
+
+	$('#comment_area').on("submit",'#new_comment', function( event ){
+		event.preventDefault();
+		comment = $("#new_comment textarea").val();
+		author = $("#authorName").val();
+
+		$("#new_comment").hide();
+
+		addComment(comment, author);
+
+		$('#new_comment_button').show();
+	});
+
 
 });
+
+
+function addForm() {
+	$("#comment_list").append('<form id="new_comment" action="" method="POST">');
+	$("#comment_list form").append('<textarea placeholder="Comment">');
+	$("#comment_list form").append('<input id="authorName" placeholder="Author">');
+	$("#comment_list form").append('<input type="submit">');
+}
+
+function addComment(comment, author) {
+	$("#comment_list").append('<li>' + comment + '<span class="author">' + author + '</span>' + '</li>');
+}
