1d355a67f9f49f66459023a5095a63de0d216788
codeamac02 <codeamac02@gmail.com>
2015-07-07 13:50:02 -0500

    Add validation presence to comment and author

--- public/js/application.js
+++ public/js/application.js
@@ -8,14 +8,22 @@ $(function(){
 
 	$('#comment_area').on("submit",'#new_comment', function( event ){
 		event.preventDefault();
+		$("#error").html("");
 		comment = $("#new_comment textarea").val();
 		author = $("#authorName").val();
+		// validatePost(author);
 
-		$("#new_comment").hide();
 
-		addComment(comment, author);
+		if (validateAuthor(author) && validateComment(comment)) {
+			addComment(comment, author);
+			$("#new_comment").hide();
+			$('#new_comment_button').show();
+		} else {
+			$("#error").html("<p>Todos los campos deben estar llenos.</p>");
 
-		$('#new_comment_button').show();
+		}
+
+
 	});
 
 
@@ -24,11 +32,20 @@ $(function(){
 
 function addForm() {
 	$("#comment_list").append('<form id="new_comment" action="" method="POST">');
-	$("#comment_list form").append('<textarea placeholder="Comment">');
+	$("#comment_list form").append('<textarea placeholder="Comment"></textarea>');
 	$("#comment_list form").append('<input id="authorName" placeholder="Author">');
+	$("#comment_list form").append('<div id="error"></div>');
 	$("#comment_list form").append('<input type="submit">');
 }
 
 function addComment(comment, author) {
 	$("#comment_list").append('<li>' + comment + '<span class="author">' + author + '</span>' + '</li>');
+}
+
+function validateAuthor(author){
+	return author.length != 0 
+}
+
+function validateComment(comment){
+	return comment.length != 0 
 }
